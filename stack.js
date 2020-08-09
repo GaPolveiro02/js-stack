57cf12458e58a5598682f66468a1b742b5adcb1e
Gabriel Polveiro <gabriel-polveiro@hotmail.com>
2020-08-08 22:58:37 -0300

    JavaScript stack methods

---
+++ stack.js
@@ -0,0 +1,24 @@
+const stackArr = []; // empty array
+
+const stackObj = {
+    stackAdd: (arr, item) => {
+        arr.push(item); // it will push the passed item into the array(last position)
+    },
+    stackRemove: (arr) => {
+        arr.pop(); // it will pop(remove) always the last item of the array
+    },
+    stackGetMinValeu: (arr) => {
+        return arr.reduce((acc, cur) => {
+            return (acc < cur ? acc : cur); // it will return stackArr min value
+        });
+    } 
+}
+
+for (let i = 0; i < 10 ; i++) { // push 10 random int numbers into the array stack
+    const randomNum = Math.round(Math.random() * 10);
+  
+    stackObj.stackAdd(stackArr, randomNum);
+}
+
+stackObj.stackRemove(stackArr); // remove the last stack item
+stackObj.stackGetMinValeu(stackArr); // return the min value inside the stack
